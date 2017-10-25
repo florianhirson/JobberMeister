cd62fd3c5f8ce4a38fb6fa5b1225953c176ef467
Florian <flohirson@gmail.com>
2017-10-25 23:21:05 +0200

    Update Android studio

--- app/src/main/java/fr/hirsonf/jobbermeister/RegisterProfileActivity.java
+++ app/src/main/java/fr/hirsonf/jobbermeister/RegisterProfileActivity.java
@@ -2,14 +2,18 @@ package fr.hirsonf.jobbermeister;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 /**
  * Created by flohi on 25/10/2017.
@@ -19,6 +23,7 @@ public class RegisterProfileActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     EditText lastName, firstName, date;
     Button b;
+    String ss;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,27 @@ public class RegisterProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 int selectedId = radioGroup.getCheckedRadioButtonId();
                 System.out.println("test : "+ radioGroup.getCheckedRadioButtonId());
+
+                ss = date.getText().toString();
+
+                if (date.getText().toString().length() == 10 &&(ss.charAt(2) == '/') && (ss.charAt(5) == '/')) {
+
+
+                } else {
+                    AlertDialog.Builder b = new AlertDialog.Builder(RegisterProfileActivity.this);
+                    b.setMessage("Le format de la date incorrect");
+                    b.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                        }
+                    });
+                    AlertDialog a = b.create();
+                    a.show();
+
+
+                }
+                ss = "";
 
                 if(lastName.getText().toString().trim().equals("") || firstName.getText().toString().trim().equals("")
                         || date.getText().toString().trim().equals("") || selectedId == -1) {
