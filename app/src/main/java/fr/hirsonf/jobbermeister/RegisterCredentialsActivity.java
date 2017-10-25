9a3b89ae5892cd4215b90921df8b9a2bb60afdc3
C'est Rémi <remi.dujardin@live.fr>
2017-10-25 16:33:32 +0200

    Alerte

--- app/src/main/java/fr/hirsonf/jobbermeister/RegisterCredentialsActivity.java
+++ app/src/main/java/fr/hirsonf/jobbermeister/RegisterCredentialsActivity.java
@@ -1,8 +1,10 @@
 package fr.hirsonf.jobbermeister;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -26,13 +28,18 @@ public class RegisterCredentialsActivity extends AppCompatActivity {
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mail.getText().toString().trim().equals(""))
-                    mail.setError("Veuillez entrer le mail");
+                if(mail.getText().toString().trim().equals("") || password.getText().toString().trim().equals("")) {
+                    AlertDialog.Builder b = new AlertDialog.Builder(RegisterCredentialsActivity.this);
+                    b.setMessage("Veuillez completer tous les champs !");
+                    b.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
 
-                if(password.getText().toString().trim().equals(""))
-                    password.setError("Veuillez entrer le mdp");
-
-                if(!mail.getText().toString().trim().equals("") && !password.getText().toString().trim().equals("")) {
+                        }
+                    });
+                    AlertDialog a = b.create();
+                    a.show();
+                } else {
                     Intent homepage = new Intent(RegisterCredentialsActivity.this, RegisterProfileActivity.class);
                     startActivity(homepage);
                 }
