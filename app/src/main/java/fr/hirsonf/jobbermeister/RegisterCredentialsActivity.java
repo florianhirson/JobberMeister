a1312345d34d37914716c12d63ffab088d6dbe51
Florian <flohirson@gmail.com>
2017-10-25 16:20:02 +0200

    Dorénavant il est nécessaire de remplir les champs pour continuer

--- app/src/main/java/fr/hirsonf/jobbermeister/RegisterCredentialsActivity.java
+++ app/src/main/java/fr/hirsonf/jobbermeister/RegisterCredentialsActivity.java
@@ -1,12 +1,18 @@
 package fr.hirsonf.jobbermeister;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegisterCredentialsActivity extends AppCompatActivity {
+
+    EditText mail;
+    TextInputEditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +20,23 @@ public class RegisterCredentialsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register_credentials);
 
         Button b = (Button) findViewById(R.id.b_next);
+        mail = (EditText) findViewById(R.id.editTextEmail);
+        password = (TextInputEditText) findViewById(R.id.editTextPassword);
 
         b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent homepage = new Intent(RegisterCredentialsActivity.this, RegisterProfileActivity.class);
-                startActivity(homepage);
+                if(mail.getText().toString().trim().equals(""))
+                    mail.setError("Veuillez entrer le mail");
+
+                if(password.getText().toString().trim().equals(""))
+                    password.setError("Veuillez entrer le mdp");
+
+                if(!mail.getText().toString().trim().equals("") && !password.getText().toString().trim().equals("")) {
+                    Intent homepage = new Intent(RegisterCredentialsActivity.this, RegisterProfileActivity.class);
+                    startActivity(homepage);
+                }
+
             }
         });
     }
--- app/src/main/java/fr/hirsonf/jobbermeister/RegisterCriteriaActivity.java
+++ app/src/main/java/fr/hirsonf/jobbermeister/RegisterCriteriaActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 /**
  * Created by flohi on 25/10/2017.
@@ -16,6 +18,20 @@ public class RegisterCriteriaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_criteria);
+
+        Spinner spDomain = (Spinner) findViewById(R.id.spinnerDomain);
+        ArrayAdapter<String> spDomainArrayAdapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.domains)); //selected item will look like a spinner set from XML
+        spDomainArrayAdapter.setDropDownViewResource(android.R.layout
+                .simple_spinner_dropdown_item);
+        spDomain.setAdapter(spDomainArrayAdapter);
+
+        Spinner spType = (Spinner) findViewById(R.id.spinnerContracts);
+        ArrayAdapter<String> spTypeArrayAdapter = new ArrayAdapter<>
+                (this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.contracts)); //selected item will look like a spinner set from XML
+        spTypeArrayAdapter.setDropDownViewResource(android.R.layout
+                .simple_spinner_dropdown_item);
+        spType.setAdapter(spTypeArrayAdapter);
 
         Button b = (Button) findViewById(R.id.b_next);
 
