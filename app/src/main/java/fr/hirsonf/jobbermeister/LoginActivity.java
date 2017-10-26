377ede1baa964577657c9742b293b5274497082a
Florian <flohirson@gmail.com>
2017-10-26 09:29:30 +0200

    Verification de l'email pour la page de login

--- app/src/main/java/fr/hirsonf/jobbermeister/LoginActivity.java
+++ app/src/main/java/fr/hirsonf/jobbermeister/LoginActivity.java
@@ -1,10 +1,19 @@
 package fr.hirsonf.jobbermeister;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static fr.hirsonf.jobbermeister.RegisterCredentialsActivity.isEmailValid;
 
 /**
  * Created by flohi on 26/10/2017.
@@ -13,6 +22,8 @@ import android.widget.Button;
 public class LoginActivity extends AppCompatActivity {
 
     Button login, register;
+    EditText email;
+    TextInputEditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +32,66 @@ public class LoginActivity extends AppCompatActivity {
 
         login = findViewById(R.id.b_login);
         register = findViewById(R.id.b_register);
+        email =  findViewById(R.id.editTextEmail);
+        password =  findViewById(R.id.editTextPassword);
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent homepage = new Intent(LoginActivity.this, RegisterCredentialsActivity.class);
-                startActivity(homepage);
+                if(checkFields()) {
+                    Intent homepage = new Intent(LoginActivity.this, RegisterCredentialsActivity.class);
+                    startActivity(homepage);
+                }
             }
         });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkFields()) {
+
+                }
+            }
+        });
+
+    }
+
+    public static boolean isEmailValid(CharSequence email) {
+        Pattern pattern;
+        Matcher matcher;
+        String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public boolean checkFields() {
+        if(email.getText().toString().trim().equals("") || password.getText().toString().trim().equals("")) {
+            AlertDialog.Builder b = new AlertDialog.Builder(LoginActivity.this);
+            b.setMessage("Veuillez completer tous les champs !");
+            b.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            AlertDialog a = b.create();
+            a.show();
+            return false;
+        } else if(!isEmailValid(email.getText())) {
+            AlertDialog.Builder b = new AlertDialog.Builder(LoginActivity.this);
+            b.setMessage("Le format de l'adresse email est invalide !");
+            b.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                }
+            });
+            AlertDialog a = b.create();
+            a.show();
+            return false;
+        }
+        return true;
     }
 }
