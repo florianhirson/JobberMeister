865ca2531655ec5a29f2613cb9b4b3f26b5078f2
Florian <flohirson@gmail.com>
2017-10-26 13:18:13 +0200

    Update

--- app/src/main/java/fr/hirsonf/jobbermeister/DashboardActivity.java
+++ app/src/main/java/fr/hirsonf/jobbermeister/DashboardActivity.java
@@ -1,8 +1,10 @@
 package fr.hirsonf.jobbermeister;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 
 import com.mindorks.placeholderview.SwipeDecor;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
@@ -25,9 +27,14 @@ public class DashboardActivity extends AppCompatActivity {
         mSwipeView = (SwipePlaceHolderView)findViewById(R.id.swipeView);
         mContext = getApplicationContext();
 
+        int bottomMargin = Utils.dpToPx(160);
+        Point windowSize = Utils.getDisplaySize(getWindowManager());
         mSwipeView.getBuilder()
                 .setDisplayViewCount(3)
                 .setSwipeDecor(new SwipeDecor()
+                        .setViewWidth(windowSize.x)
+                        .setViewHeight(windowSize.y - bottomMargin)
+                        .setViewGravity(Gravity.TOP)
                         .setPaddingTop(20)
                         .setRelativeScale(0.01f)
                         .setSwipeInMsgLayoutId(R.layout.ad_swipe_in_msg_view)
